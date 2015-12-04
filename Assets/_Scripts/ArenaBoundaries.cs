8e029506f15b0a72e59b31464b345882a8dec139
Daankovski <danielepke@hotmail.com>
2015-12-04 22:40:21 +0100

    Commit: Pushed forgotten scripts

---
+++ Assets/_Scripts/ArenaBoundaries.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArenaBoundaries : MonoBehaviour {
+
+    
+    void Start () {
+        
+	}
+
+	void Update () {
+	
+	}
+
+    void OnTriggerExit(Collider other) {
+        GameObject Player = GameObject.FindGameObjectWithTag("Player");
+        PlayerMovement player = Player.GetComponent<PlayerMovement>();
+       
+        if (other.gameObject.tag == "Player") {
+            Debug.Log("Speler is uit het veld");
+            player.playerHealth = 0;
+        }
+    }
+}
