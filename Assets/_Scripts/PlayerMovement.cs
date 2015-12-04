0c177f1cf05bdc3b20f53527f757ebf11c6224e8
Daankovski <danielepke@hotmail.com>
2015-12-04 22:37:23 +0100

    Commit: Player is able to rotate to Right Stick location; Started working slippery / ice effect.

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -3,30 +3,59 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-    private Vector3 movementVector;
     private CharacterController characterController;
+
+    // Movement Related Variables
+    private Vector3 movementVector;
     private float f_movementSpeed = 8f;
     private float f_jumpPower = 15f;
     private float f_gravity = 40f;
+    private float f_friction;
+    private float f_velocity = 1f;
 
+    // Joystick Related Variables
     [SerializeField]
     private int i_joystickNumber;
 
-	// Use this for initialization
+    // Player Related Variables
+    [SerializeField]
+    private int f_playerHealth = 100;
+	
 	void Start () {
         characterController = GetComponent<CharacterController>();
 	}
+
+    // Getters & Setters
+    public int playerHealth {
+        get { return f_playerHealth; }
+        set { f_playerHealth = value; }
+    }
 	
-	// Update is called once per frame
+	
 	void Update () {
-        Movement();
+        PlayerControlls();
     }
 
-    void Movement() {
-        string joystickString = i_joystickNumber.ToString();
-        movementVector.x = Input.GetAxis("LeftJoystickX_P" + joystickString) * f_movementSpeed;
-        movementVector.z = Input.GetAxis("LeftJoystickY_P" + joystickString) * f_movementSpeed;
+    void FixedUpdate() {
+        Debug.Log(playerHealth);
+    }
 
+    void PlayerControlls() {
+        // Horizontal & Vertical Movement
+        string joystickString = i_joystickNumber.ToString();
+
+        float f_leftStick_X = Input.GetAxis("LeftJoystickX_P" + joystickString) * f_movementSpeed;
+        float f_leftStick_Y = Input.GetAxis("LeftJoystickY_P" + joystickString) * -f_movementSpeed;
+
+        movementVector = new Vector3(f_leftStick_X, movementVector.y, f_leftStick_Y);
+
+        // Rotation
+        var horz = Input.GetAxis("RightJoystickX_P" + joystickString);
+        var vert = Input.GetAxis("RightJoystickY_P" + joystickString);
+        var aimDirection = Mathf.Atan2(vert, horz) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, aimDirection, 0);
+
+        // Jumping
         if (characterController.isGrounded)
         {
             movementVector.y = 0;
@@ -38,5 +67,11 @@ public class PlayerMovement : MonoBehaviour {
 
         movementVector.y -= f_gravity * Time.deltaTime;
         characterController.Move(movementVector * Time.deltaTime);
+    }
+
+    void PlayerDeath() {
+        if (playerHealth <= 0) {
+            Destroy(this.gameObject);
+        }
     }
 }
